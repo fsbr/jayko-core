f3c2388ed320cf9efdde0de651e9546a182d6efc
Thomas Fuller <thomasckfuller@gmail.com>
2025-08-29 01:35:40 -0700

    first unit tests

---
+++ test_jayko.py
@@ -0,0 +1,61 @@
+import subprocess
+import tempfile
+import os
+# module for writing unit tests 
+def add(x,y):
+    return x+y
+
+def test_add_positive_numbers():
+    assert add(2, 3) == 5
+    assert add(0, 0) == 0
+
+def test_compile_say1():
+    result = subprocess.run (
+        ["python3", "jayko.py", "jko_src/say1.jko"],
+        capture_output=True,
+        text=True
+    )
+    c_result = subprocess.run (
+        ["./output"],
+        capture_output=True,
+        text=True
+    )
+    #print(result.stdout)
+    #print(repr(c_result.stdout))
+    #print(c_result.stderr)
+    assert result.returncode == 0
+    assert c_result.stdout == "1\n69\n-68hello\n24\n1\n"
+
+def test_compile_bf():
+    result = subprocess.run (
+        ["python3", "jayko.py", "jko_src/bf.jko"],
+        capture_output=True,
+        text=True
+    )
+    c_result = subprocess.run (
+        ["./output"],
+        capture_output=True,
+        text=True
+    )
+    #print(result.stdout)
+    print(repr(c_result.stdout))
+    #print(c_result.stderr)
+    assert result.returncode == 0
+    assert c_result.stdout == 'STARTING INSTRUCTION ANALYSIS H e l l o   W o r l d ! \n 0 0 72 100 87 33 10 0 0 0 0 0 0 0 0 0 0 0 0 0 COMPLETE\n '
+
+def test_compile_fizzbuzz():
+    result = subprocess.run (
+        ["python3", "jayko.py", "jko_src/fizzbuzz.jko"],
+        capture_output=True,
+        text=True
+    )
+    c_result = subprocess.run (
+        ["./output"],
+        capture_output=True,
+        text=True
+    )
+    #print(result.stdout)
+    print(repr(c_result.stdout))
+    #print(c_result.stderr)
+    assert result.returncode == 0
+    assert c_result.stdout == '3Fizz5Buzz6Fizz9Fizz10Buzz12Fizz15FizzBuzz18Fizz20Buzz21Fizz24Fizz25Buzz27Fizz30FizzBuzz'
